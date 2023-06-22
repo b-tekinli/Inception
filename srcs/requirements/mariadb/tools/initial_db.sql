99244edd57be704fa4a5887b33ea66e73c580eef
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-06-22 17:41:10 +0300

    added

---
+++ srcs/requirements/mariadb/tools/initial_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'btekinli'@'%' IDENTIFIED BY 'force99';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'btekinli'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootforce99';
